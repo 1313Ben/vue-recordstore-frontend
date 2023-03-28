e7344f6b5a3a2ea870bea2d5719c0ca9c0a72136
1313Ben <ravindra_github@bluewin.ch>
2023-03-28 09:19:06 +0200

    Signin Component

--- src/components/Signin.vue
+++ src/components/Signin.vue
@@ -9,12 +9,69 @@
 
       <div class="mb-6">
         <label for="email">E-mail</label>
+        <input type="email" v-model="email" class="input" id="email" placeholder="a@a.com">
       </div>
+
+      <div class="mb-6">
+        <label for="password">Password</label>
+        <input type="password" v-model="password" class="input" id="password" placeholder="password">
+      </div>
+
+      <button type="submit" >
+        Sign In
+      </button>
+
+      <div class="my-4">
+        <router-link to="/signup" class="link">Sign Up</router-link>
+      </div>
+
     </form>
   </div>
 </div>
 </template>
 
 <script>
-
+export default {
+  name: 'Signin',
+  data () {
+    return {
+      email: '',
+      password: '',
+      error: ''
+    }
+  },
+  created () {
+    this.checkSignedIn()
+  },
+  updated () {
+    this.checkSignedIn()
+  },
+  methods: {
+    signin () {
+      this.$http.plain.post('/signin', {email: this.email, password: this.password})
+        .then(response => this.signinSuccessful(response))
+        .catch(error => this.signinFailed(error))
+    },
+    signinSuccessful (response) {
+      if (!response.data.csrf) {
+        this.signinFailed(response)
+        return
+      }
+      localStorage.csrf = response.data.csrf
+      localStorage.signedIn = true
+      this.error = ''
+      this.$router.replace('/records')
+    },
+    signinFailed (error) {
+      this.error = (error.response && error.response.data && error.response.data.error) || ''
+      delete localStorage.csrf
+      delete localStorage.signedIn
+    },
+    checkSignedIn () {
+      if (localStorage.signedIn) {
+        this.$router.replace('/records')
+      }
+    }
+  }
+}
 </script>
